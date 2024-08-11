9443a80647d2814a1e4e9e329a613031d52aaa76
danchous21 <danceussisk228@gmail.com>
2024-08-12 01:10:58 +0300

    Создали первое окно

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def main():
+    # Создаем главное окно
+    root = tk.Tk()
+    root.title("Finance Tracker")
+
+    # Устанавливаем минимальный размер окна
+    root.minsize(400, 300)
+
+    # Запускаем главный цикл программы
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
